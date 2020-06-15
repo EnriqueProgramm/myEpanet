c75041ce4787c3307c29511ec46362ef19f369b8
EnriqueProgramm <campbellenrique@yahoo.com>
2020-06-15 08:27:17 +0200

    Version with node and pipe selectors

--- main.js
+++ main.js
@@ -4,6 +4,42 @@ const { get } = require('http');
 const { table } = require('console');
 
 window.onload = function () {
+const net1 = fs.readFileSync('net1.inp')
+const ws = new Workspace();
+const model = new Project(ws);
+ws.writeFile('net1.inp', net1);
+model.open('net1.inp', 'report.rpt', 'out.bin'); 
+
+
+const amountNodes = model.getCount(0);
+const arrayNamesNodes = []; 
+for (var i=1; i<= amountNodes; i++){
+  arrayNamesNodes.push(model.getNodeId(i));
+};
+var select = document.getElementById("selectNode")
+for(var i = 0; i< arrayNamesNodes.length; i++){
+      var option = document.createElement("OPTION"),
+      txt = document.createTextNode(arrayNamesNodes[i]);
+      option.appendChild(txt);
+      option.setAttribute("value", arrayNamesNodes[i]);
+      select.insertBefore(option, select.lastChild);
+  }
+
+
+const amountPipes = model.getCount(2);
+const arrayNamesPipes = []; 
+for (var i=1; i<= amountPipes; i++){
+    arrayNamesPipes.push(model.getLinkId(i));
+};
+var select = document.getElementById("selectPipe")
+for(var i = 0; i< arrayNamesPipes.length; i++){
+        var option = document.createElement("OPTION"),
+        txt = document.createTextNode(arrayNamesPipes[i]);
+        option.appendChild(txt);
+        option.setAttribute("value", arrayNamesPipes[i]);
+        select.insertBefore(option, select.lastChild);
+    }
+  
 
 document.getElementById('simulation').onclick = function (){
   const data = runSimulation();
@@ -13,12 +49,10 @@ document.getElementById('simulation').onclick = function (){
   const xValues = [];
   const yValues = [];
   tableValues.forEach(element => {
-    xValues.push((element).toFixed(2));
-    console.log(element);
+    xValues.push((element).toFixed(2));    
   });
   tableTimes.forEach(element => {
     yValues.push((element));
-    console.log(element);
   });  
   
   var ctx = document.getElementById('myChart').getContext('2d');
@@ -53,18 +87,11 @@ document.getElementById('simulation').onclick = function (){
 
 }
 
+  
 //runSimulation();
-function runSimulation() {
-
-
-const net1 = fs.readFileSync('net1.inp')
-
-const ws = new Workspace();
-const model = new Project(ws);
-
-ws.writeFile('net1.inp', net1);
-model.open('net1.inp', 'report.rpt', 'out.bin');
-const nodeName = '11'
+function runSimulation() { 
+var currentSelection = document.getElementById("selectNode").value
+const nodeName = currentSelection.toString();
 const n11Index = model.getNodeIndex(nodeName);
 model.openH();
 model.initH(11);
